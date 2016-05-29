ecfdd615081ffd419812db57babcdb20ad9c138b
Kornel Kovacs <kornel.kovacs@draftgate.com>
2016-05-29 12:48:46 +0200

    init

---
+++ data/sites/default/config.inc.php
@@ -0,0 +1,22 @@
+<?php
+/**************************************************************************
+* Database configuration file for WebIssues Server
+*
+* Site name: default
+* Base URL address: http://10.0.0.2/webissues-server/
+*
+* This file was generated on 2016-05-29 using version 1.1.4.
+*
+* NOTE: Do not modify this file unless you know what you are doing.
+* You can delete it and run the setup script to create a new configuration
+* file.
+**************************************************************************/
+
+if ( !defined( 'WI_VERSION' ) ) die( -1 );
+
+$config[ 'db_engine' ] = 'mysqli';
+$config[ 'db_host' ] = 'localhost';
+$config[ 'db_database' ] = 'webissues';
+$config[ 'db_user' ] = 'root';
+$config[ 'db_password' ] = '12345';
+$config[ 'db_prefix' ] = '';
